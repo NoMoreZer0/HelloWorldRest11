dd88a5659fb270970decfaa2615271b80c618ec2
NoMoreZer0 <kazakupersuper@mail.ru>
2023-10-26 17:43:50 +0600

    changed hello world

--- src/main/java/com/example/helloworldprojectmaven11/HelloWorldController.java
+++ src/main/java/com/example/helloworldprojectmaven11/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping
     public String getHelloWorld() {
-        return "hello world...";
+        return "changed hello world...";
     }
 }
