e965d7744d4cfa64ceb221144ca3075a173d127a
NoMoreZer0 <kazakupersuper@mail.ru>
2023-10-26 16:47:00 +0600

    first commit

---
+++ src/main/java/com/example/helloworldprojectmaven11/HelloWorldProjectMaven11Application.java
@@ -0,0 +1,13 @@
+package com.example.helloworldprojectmaven11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProjectMaven11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldProjectMaven11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldprojectmaven11/HelloWorldProjectMaven11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldprojectmaven11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldProjectMaven11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
